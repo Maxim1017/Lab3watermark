b96bd3012ea512d16d63263a0179c0c94dd1a90c
ivanshin <48406105+ivanshin@users.noreply.github.com>
2020-11-27 13:40:15 +0300

    Update save_instruments.py

--- save_instruments.py
+++ save_instruments.py
@@ -147,3 +147,10 @@ def add_to_edited_table(file_name):
         print("Oops!", e.__class__, "occurred.")
         
 
+def test():
+    print('hi')
+    add_to_origin_table('image.png')
+    add_to_edited_table('image.png')
+    im = Image.open('image.png')
+    save_original_image(im, 'im.png')
+    save_edited_image(im, 'im.png')
