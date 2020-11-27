d81b1ee64ab260e238296d5d4dfd891e0623266b
krivichanin <48236346+krivichanin@users.noreply.github.com>
2020-11-27 13:44:31 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -18,3 +18,9 @@ def get_watermark_image(imagename):
 		img = cv2.imread(imagename)
 		_, img_encoded = cv2.imencode('.jpg', img)
 	
+
+		r= requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+		r.raise_for_status()
+		nparr = np.fromstring(r.content, np.uint8)
+		im = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+		return im
